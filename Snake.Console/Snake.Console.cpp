5c0ffeecba29b2a2379adcce0e54273f4ece03bc
Dallan Simper <dallan.simper@gmail.com>
2024-04-05 08:31:03 -0600

    Update Snake.Console.cpp
    code from other attempt

--- Snake.Console/Snake.Console.cpp
+++ Snake.Console/Snake.Console.cpp
@@ -1,21 +1,133 @@
-// Snake.Console.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <ctime>
+#include <chrono>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    
+#define key_UP 72
+#define key_DOWN 80
+#define key_LEFT 75
+#define key_RIGHT 77
+#define key_ESCAPE 27
+
+//This is a struct (aka structure) it reduces the number of variables needed.
+struct point { int x, y; };
+
+//keypress keeps track of what the last pressed key. It is global, but could be made local.
+int keypress = 0;
+
+void runGame();
+point keyPressed(point direction);
+void gotoxy(int x, int y);
+void setcolor(WORD color);
+void txtPlot(point item, unsigned char Color);
+
+//Add function declarations here
+
+
+int main() {
+
+    //Uncomment the next four lines to see console color options 0-255
+    //for (int i = 0; i < 255; i++) {
+    //    setcolor(i);
+    //    _cprintf("Color option: %i. Hello, world.\n", i);
+    //}
+
+    runGame();
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void runGame() {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    chrono::time_point<chrono::system_clock> runTime;
+    chrono::time_point<chrono::system_clock> currentTime;
+    runTime = std::chrono::system_clock::now();
+    Sleep(300);
+
+    point playerloc = { 0, 10 };
+    point direction = { 1, 0 };
+    int length = 5;
+
+    //Loop to start drawing and playing.
+    //while (keypress != key_ESCAPE) {
+
+    direction = keyPressed(direction);
+
+    currentTime = chrono::system_clock::now();
+
+    double elapsedTime = chrono::duration_cast<chrono::milliseconds>(currentTime - runTime).count();
+    if (elapsedTime > 0.3 * 1000) {
+        runTime = chrono::system_clock::now();
+
+        //Most of your game logic goes here.
+
+        txtPlot(playerloc, 31);
+
+        setcolor(15);
+        gotoxy(1, 21);
+        _cprintf("Length: %i", length);
+
+
+    }
+
+    Sleep(10);
+    //}
+}
+
+//Put function definitions here.
+
+
+
+//These are helper funcitons to capture keyboard and draw to the console.
+point keyPressed(point direction) {
+    if (_kbhit())
+    {
+        keypress = _getch();
+        switch (keypress)
+        {
+        case key_LEFT:
+            direction = { -1, 0 };
+            break;
+
+        case key_RIGHT:
+            direction = { 1, 0 };
+            break;
+
+        case key_UP:
+            direction = { 0, -1 };
+            break;
+
+        case key_DOWN:
+            direction = { 0, 1 };
+            break;
+
+        default:
+
+            break;
+        }
+    }
+    return direction;
+}
+
+void txtPlot(point item, unsigned char Color)
+{
+    setcolor(Color);
+    gotoxy(item.x * 2, item.y);
+    _cprintf("  ");
+}
+
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x; coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    return;
+}
+
+void setcolor(WORD color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    return;
+}
